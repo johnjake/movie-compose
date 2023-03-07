95cc6c2b2439d88b4131e76532a32c06ddd814c1
john jake talledo <johnjake.talledo2000@gmail.com>
2023-03-07 11:12:23 +0800

    dsl and hilt dagger

---
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -0,0 +1,6 @@
+object Version {
+    const val kotlin_version = "1.8.10"
+    const val compose_version = "1.4.2"
+    const val gradle_version = "7.4.2"
+    const val dagger_version = "2.44"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "Cinema Guide"
+include(":app")
